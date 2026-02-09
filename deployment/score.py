c2b44cdc89079e9642eeb59e58b942462365ece2
vamsidhar1 <93872691+vamsidhar1@users.noreply.github.com>
2026-02-09 17:36:29 +0530

    Update score.py

--- deployment/score.py
+++ deployment/score.py
@@ -1,1 +1,14 @@
+import json
+import joblib
+import os
+
+def init():
+    global model
+    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model/model.pkl")
+    model = joblib.load(model_path)
+
+def run(raw_data):
+    data = json.loads(raw_data)
+    result = model.predict(data)
+    return result.tolist()
 
